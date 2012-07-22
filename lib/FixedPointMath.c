8dbf4ed4981c786aad81c510af948bc9f2bf7fc8
Mircea <Mircea@Mircea-PC>
2012-07-22 23:47:43 +0100

    Added functional iteration division for 16.16

--- lib/FixedPointMath.c
+++ lib/FixedPointMath.c
@@ -18,6 +18,18 @@ short norm8_8(int8_8 x)
 	return (pos + last);
 }
 
+short norm16_16(int16_16 x)
+{
+	if (!x) return 0;
+	short pos = 28;
+	char last = 0;
+	while (!(last=bits4_most_significant(x >> 28))){
+		x <<= 4;
+		pos -= 4;
+	}
+	return (pos + last);
+}
+
 int8_8 div8_8_v2(int8_8 n,int8_8 m)
 {
     char sign = 1;
@@ -38,7 +50,7 @@ int8_8 div8_8_v2(int8_8 n,int8_8 m)
 	  x = ((((b*x)>>32)*x)>>32) + ((b*x)>>31) + b;
 	  x = p-x;
 	  x &=0x00000000FFFFFFFF;
-	  printf("---x:%d %x\n",x_i,x);
+	  //printf("---x:%d %x\n",x_i,x);
 	 /* p_i = p>>32 ;
       x = ((x*(~p+1))>>32)+((2-p_i)*x)-x_i*p;
       x_i = x >> 32;
@@ -50,7 +62,7 @@ int8_8 div8_8_v2(int8_8 n,int8_8 m)
 	x >>=32;
 
 	if (sign == -1) x = ~x;
-	printf("---x:%x\n",x);
+	//printf("---x:%x\n",x);
 	return (int8_8)x;
 }
 
@@ -87,6 +99,41 @@ int8_8 div8_8(int8_8 n, int8_8 m)
     if (sign == -1) out = ~out;
    // printf("return:%x %d\n",(out),out);
 	return (int8_8)(out);
+};
+
+int16_16 div16_16(int16_16 n, int16_16 m)
+{
+    char sign = 1;
+	if (m<0) {m = -m; sign = -1;}
+	if (n<0) {n = -n; sign *= -1;}
+	short k = norm16_16(m);
+	if (!k) return 0;
+
+
+	uint64_t y = -(((int32_t)m) << (32-k));
+	uint64_t x = -(((int32_t)m) << (32-k));
+	if (y==2147483648) return (k>8?n>>(k-9):n<<(9-k));
+	int j;
+	uint64_t p=1ULL;
+
+	float mm = (((int32_t)m) << (32-k))/65536.0;
+	//printf("m:%f\n",mm);
+	for (j=0;j<4;j++){
+    p = x*x;
+	x = p >> 32;
+    p = x*y ;
+    y = (p >> 32)+x+y;
+	}
+
+	uint64_t res = y | (0x0000000100000000);
+	uint64_t out = res * n;
+
+    out = (k>16?out>>(k-16):out<<(16-k));
+    //out += (1<<15);
+    out >>=32;
+    if (sign == -1) out = ~out;
+    //printf("return:%x\n",out);
+	return (int16_16)(out);
 };
 
 inline char bits4_most_significant(char x)
--- test_sources/FixedPointMathTests.h
+++ test_sources/FixedPointMathTests.h
@@ -8,9 +8,9 @@
 #ifndef FIXEDPOINTMATHTESTS_H_
 #define FIXEDPOINTMATHTESTS_H_
 
-char* test_div_8_8()
+char* test_div_8_8_v2()
 {
-	  mu_test_title("Divide for 8.8");
+	  mu_test_title("Divide for 8.8 - Newton Raphson");
 	  init_file("tests/gen/8_8/divide.test");
 	  int8_8 x1,x2,y;
 	  Parsed_fixed_point input1;
@@ -41,6 +41,76 @@ char* test_div_8_8()
 
 	  return 0;
 }
+
+char* test_div_8_8()
+{
+	  mu_test_title("Divide for 8.8");
+	  init_file("tests/gen/8_8/divide.test");
+	  int8_8 x1,x2,y;
+	  Parsed_fixed_point input1;
+	  Parsed_fixed_point input2;
+	  Parsed_fixed_point output;
+	  int i=0;
+	  while (get_operand(i,1)!=NULL)
+	  {
+	  	input1=*(Parsed_fixed_point*)get_operand(i,1);
+	  	input2=*(Parsed_fixed_point*)get_operand(i,2);
+	  	output=*(Parsed_fixed_point*)get_result(i);
+
+	  	(output.fractional_part) >>=24;
+	  	(input1.fractional_part) >>=24;
+	  	(input2.fractional_part) >>=24;
+	  	x1 = set_int_part_8_8(x1,input1.integer_part);
+		x1 = set_fract_part_8_8(x1,input1.fractional_part);
+	  	x2 = set_int_part_8_8(x2,input2.integer_part);
+		x2 = set_fract_part_8_8(x2,input2.fractional_part);
+
+	  	y = div8_8(x1,x2);
+	 	mu_assert_line("error",i,((get_int_part_8_8(y)==output.integer_part)&&(get_fract_part_8_8(y)==output.fractional_part)));
+	  	i++;
+	  	}
+
+
+	  mu_final();
+
+	  return 0;
+}
+
+char* test_div_16_16()
+{
+	  mu_test_title("Divide for 16.16");
+	  init_file("tests/gen/16_16/divide.test");
+	  int16_16 x1,x2,y;
+	  Parsed_fixed_point input1;
+	  Parsed_fixed_point input2;
+	  Parsed_fixed_point output;
+	  int i=0;
+	  while (get_operand(i,1)!=NULL)
+	  {
+	  	input1=*(Parsed_fixed_point*)get_operand(i,1);
+	  	input2=*(Parsed_fixed_point*)get_operand(i,2);
+	  	output=*(Parsed_fixed_point*)get_result(i);
+
+	  	(output.fractional_part) >>=16;
+	  	(input1.fractional_part) >>=16;
+	  	(input2.fractional_part) >>=16;
+	  	x1 = set_int_part_16_16(x1,input1.integer_part);
+		x1 = set_fract_part_16_16(x1,input1.fractional_part);
+	  	x2 = set_int_part_16_16(x2,input2.integer_part);
+		x2 = set_fract_part_16_16(x2,input2.fractional_part);
+
+	  	y = div16_16(x1,x2);
+	 	mu_assert_line("error",i,((get_int_part_16_16(y)==output.integer_part)&&(get_fract_part_16_16(y)==output.fractional_part)));
+	  	//printf("diff:%d %d\n",get_int_part_16_16(y)-output.integer_part,get_fract_part_16_16(y)-output.fractional_part);
+	  	i++;
+	  	}
+
+
+	  mu_final();
+
+	  return 0;
+}
+
 char* test_bits4_most_significant()
 {
 	mu_test_title("Testing detecting most significant bit");
@@ -84,6 +154,8 @@ char* test_norm_8_8()
 
 char * all_tests_fixed_point_math() {
 	mu_run_test(test_div_8_8);
+	mu_run_test(test_div_8_8_v2);
+	mu_run_test(test_div_16_16);
 	mu_run_test(test_bits4_most_significant);
 	mu_run_test(test_norm_8_8);
 	return 0;
